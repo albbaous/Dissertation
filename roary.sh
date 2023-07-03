7edb300d7f4b40780314b0a8cdc741a953b9bc67
albbaous <83087466+albbaous@users.noreply.github.com>
2023-07-03 15:13:54 +0100

    Update roary.sh
    successful for 208 out of 258

--- roary.sh
+++ roary.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=Roary
+#SBATCH --job-name=Roary_HK
 #SBATCH --partition=defq
-#SBATCH --nodes=24
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=30g
-#SBATCH --time=120:00:00
-#SBATCH --output=/gpfs01/home/styab21/OandE/%x.out
-#SBATCH --error=/gpfs01/home/styab21/OandE/%x.err
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=100g
+#SBATCH --time=168:00:00
+#SBATCH --output=/gpfs01/home/styab21/error/%x.out
+#SBATCH --error=/gpfs01/home/styab21/error/%x.err
 
-
-#Activate Conda
+# Activate Conda
 source $HOME/.bash_profile
-conda activate /gpfs01/home/styab21/miniconda3/envs/roary_env
+conda activate /gpfs01/home/styab21/miniconda3/envs/roary3_env
 
 # Run Roary
-roary -f roary_results -e -v --mafft -p 8 *.gff
+roary -f roary_results -e -v --mafft -p 40 -i 30 -s -g 400000 -iv 1.2 *.gff
